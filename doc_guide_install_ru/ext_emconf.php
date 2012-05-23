a34ca9989aa711d500063046f4766af802cfdd39
Christopher <chrissitopher@gmx.de>
2012-05-23 13:46:39 +0200

    [TASK] Migrate doc_guide_install_ru 2.1.0
    Import doc_guide_install_ru 2.1.0; last version from SVN.
    
    Change-Id: I7d1c772f20e27f36c59855fe90478f42c0f45db2

---
+++ doc_guide_install_ru/ext_emconf.php
@@ -0,0 +1,52 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_guide_install_ru".
+#
+# Auto generated 02-09-2011 09:34
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Russian translation of TYPO3 Installation and Upgrade',
+	'description' => 'Very important document describing the filestructure of TYPO3 and taking you step by step through the installation.
+Does not deal with specific servers/OSes - just general TYPO3 installation issues.',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '2.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'doNotLoadInFE' => 1,
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'0' => array(
+		),
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"671e";s:12:"ext_icon.gif";s:4:"c506";s:14:"doc/manual.odt";s:4:"a88c";s:14:"doc/manual.pdf";s:4:"ee9e";s:14:"doc/manual.sxw";s:4:"6dd3";s:14:"doc/manual.txt";s:4:"08e0";}',
+	'suggests' => array(
+	),
+);
+
+?>
