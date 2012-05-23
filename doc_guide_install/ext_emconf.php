42fb940e13b7bd72ee5367cd14333c9c80d22550
Christopher <chrissitopher@gmx.de>
2012-05-23 13:30:59 +0200

    [TASK] Migrate doc_guide_install 0.0.1
    Import doc_guide_install 0.0.1.
    
    Change-Id: I73df91b97af526d6b6050c3f47f68b5f309b951b

---
+++ doc_guide_install/ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_inst_upgr".
+#
+# Auto generated 30-11-2002 17:37
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Typo3 Installation and Upgrading',
+	'description' => 'Very important document describing the filestructure of Typo3 and taking you step by step through the installation. 
+Does not deal with specific servers/OSes - just general Typo3 installation issues.',
+	'category' => 'doc',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"doc/manual.sxw";s:4:"ccfa";s:19:"doc/wizard_form.dat";s:4:"138d";s:20:"doc/wizard_form.html";s:4:"2841";}',
+);
+
+?>
